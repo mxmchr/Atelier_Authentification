6953b3db484db0a8f662a4645e74cf3f3b3c0998
mxmchr <142352814+mxmchr@users.noreply.github.com>
2024-12-18 10:55:31 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,7 +1,7 @@
 <?php
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
-$token = bin2hex(random_bytes(16))
+$token = bin2hex(random_bytes(16));
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
