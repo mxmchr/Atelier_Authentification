f833ec20b1d6a5a5b0ae8c52f977363cd7349f81
mxmchr <142352814+mxmchr@users.noreply.github.com>
2024-12-18 11:19:36 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,11 +1,15 @@
 <?php
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
+
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-if (isset($_COOKIE['authToken'])) {
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
     header('Location: page_admin.php');
+    exit();
+} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
+    header('Location: page_user.php');
     exit();
 }
 
@@ -15,14 +19,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Vérification simple du username et de son password.
-    // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
+    // Si ok alors on initialise le cookie sur le poste de l'utilisateur
     if ($username === 'admin' && $password === 'secret') {
-        setcookie('authToken', $token, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
     } elseif ($username === 'user' && $password === 'utilisateur') {
         $token = bin2hex(random_bytes(16));
-        setcookie('authToken', $token, time() + 60, '/', '', true, true);
+        setcookie('authToken', 'user_' . $token, time() + 60, '/', '', true, true);
         header('Location: page_user.php');
         exit();
     } else {
@@ -51,6 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit">Se connecter</button>
     </form>
     <br>
-    <a href="../index.html">Retour à l'accueil</a>  
+    <a href="../index.html">Retour à l'accueil</a>
 </body>
 </html>
